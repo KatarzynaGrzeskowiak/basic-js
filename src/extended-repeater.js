45b6d266642b21e0581b0280f0152e671e5f25ea
Katarzyna Grześkowiak <tabita33@gmail.com>
2025-03-02 23:40:16 +0100

    extended complete

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // Convert str and addition to string if they are not strings
+  str = String(str);
+  options.addition = options.addition !== undefined ? String(options.addition) : '';
+  
+  // Set defaults for missing properties
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+  
+  // Create the repeated addition string
+  let addition = new Array(additionRepeatTimes).fill(options.addition).join(additionSeparator);
+  
+  // Create the final string to repeat (str + addition)
+  let result = str + addition;
+
+  // Create the final repeated string with separator
+  return new Array(repeatTimes).fill(result).join(separator);
 }
 
 module.exports = {
